9a74df14e4c6464bcd6c196fa8e96e32af21afa6
IDGS-901-22002330 <79224@alumnos.utleon.edu.mx>
2025-10-30 11:06:30 -0600

    Fix

--- app/src/main/java/com/example/allenare_mobile/model/RunningWorkout.kt
+++ app/src/main/java/com/example/allenare_mobile/model/RunningWorkout.kt
@@ -1,10 +1,12 @@
 package com.example.allenare_mobile.model
 
-import com.google.firebase.Timestamp
+import com.google.firebase.firestore.ServerTimestamp
+import java.util.Date
 
 data class RunningWorkout(
     val userId: String = "",
     val distance: Double = 0.0, // in kilometers
     val duration: Long = 0, // in seconds
-    val date: Timestamp = Timestamp.now()
+    @ServerTimestamp
+    val date: Date? = null
 )
